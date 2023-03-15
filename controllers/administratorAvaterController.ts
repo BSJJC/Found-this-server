0e896e800957586460f7daa91a44a8603f8c7ba1
BSJJC <805086021@qq.com>
2023-03-15 10:05:42 +0800

    style: added types

--- controllers/administratorAvaterController.ts
+++ controllers/administratorAvaterController.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from "express";
-import { MongoClient, GridFSBucket, ObjectId } from "mongodb";
+import {
+  MongoClient,
+  GridFSBucket,
+  ObjectId,
+  Db,
+  GridFSBucketWriteStream,
+  GridFSBucketReadStream,
+} from "mongodb";
 import asyncHandler from "express-async-handler";
 
 require("dotenv").config();
@@ -7,17 +14,20 @@ require("dotenv").config();
 const uploadAdministratorAvater = asyncHandler(
   async (req: Request, res: Response) => {
     try {
-      const client = await MongoClient.connect(process.env.MONGO_URI as string);
-      const db = client.db();
-      const imagesBucket = new GridFSBucket(db, {
+      const client: MongoClient = await MongoClient.connect(
+        process.env.MONGO_URI as string
+      );
+      const db: Db = client.db();
+      const imagesBucket: GridFSBucket = new GridFSBucket(db, {
         bucketName: "administratorAvaters",
       });
 
-      const file = req.file as Express.Multer.File;
-      const buffer = file.buffer;
-      const uploadStream = imagesBucket.openUploadStream(file.originalname);
+      const file: Express.Multer.File = req.file as Express.Multer.File;
+      const buffer: Buffer = file.buffer;
+      const uploadStream: GridFSBucketWriteStream =
+        imagesBucket.openUploadStream(file.originalname);
       uploadStream.end(buffer);
-      res.send("Image uploaded successfully");
+      res.send("administrtor avater uploaded successfully");
     } catch (err) {
       console.error(err);
       res.status(500).send("Error uploading image");
@@ -28,23 +38,24 @@ const uploadAdministratorAvater = asyncHandler(
 const getAdministratorAvater = asyncHandler(
   async (req: Request, res: Response) => {
     try {
-      const client = await MongoClient.connect(process.env.MONGO_URI as string);
+      const client: MongoClient = await MongoClient.connect(
+        process.env.MONGO_URI as string
+      );
 
-      const db = client.db();
-      const imagesBucket = new GridFSBucket(db, {
+      const db: Db = client.db();
+      const imagesBucket: GridFSBucket = new GridFSBucket(db, {
         bucketName: "administratorAvaters",
       });
 
       const imageId: ObjectId = new ObjectId(req.params.administratorAvaterId);
 
-      console.log(imageId);
-
-      const downloadStream = imagesBucket.openDownloadStream(imageId);
+      const downloadStream: GridFSBucketReadStream =
+        imagesBucket.openDownloadStream(imageId);
       res.set("Content-Type", "image/jpeg");
       downloadStream.pipe(res);
     } catch (err) {
       console.error(err);
-      res.status(500).send("Error downloading image");
+      res.status(500).send("Error fetch administrator avater");
     }
   }
 );
