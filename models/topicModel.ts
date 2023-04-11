2c3fdc2bd170bb586c2a778d8983e24b75f48661
BSJJC <805086021@qq.com>
2023-04-11 12:01:21 +0800

    chore

--- models/topicModel.ts
+++ models/topicModel.ts
@@ -6,48 +6,12 @@ import type commentType from "../types/commentType";
 interface topicModel {
   founder: string;
   title: string;
-  intro: string;
   text: string;
-  appendixId: ObjectId;
-  belong: ObjectId;
+  appendixIds: Array<string>;
   isDeleted: boolean;
-  replies: commentType[];
 }
 
-const topicModelSchema = new Schema<topicModel>({
-  founder: {
-    type: String,
-    required: [true, "Pleace add the founder"],
-  },
-  title: {
-    type: String,
-    required: [true, "Pleace add a title"],
-  },
-  intro: {
-    type: String,
-    required: [true, "Pleace add an intro"],
-  },
-  text: {
-    type: String,
-    required: [true, "Pleace add the text"],
-  },
-  appendixId: {
-    type: ObjectId,
-    required: [false, ""],
-  },
-  belong: {
-    type: ObjectId,
-    required: [true, "Pleace declear what model does this topic belongs to"],
-  },
-  isDeleted: {
-    type: Boolean,
-    required: [true, "Pleace declear if this topic is deleted"],
-  },
-  replies: {
-    type: [],
-    required: [false, ""],
-  },
-});
+const topicModelSchema = new Schema<topicModel>();
 
 const topicModel = model<topicModel>("topicModel", topicModelSchema);
 
